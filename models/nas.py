77dc8c8bc9f771193127d5e190f5f60474336c8a
MautushiD <mautushid@vt.edu>
2023-08-25 00:46:26 -0400

    model evaluation

--- models/nas.py
+++ models/nas.py
@@ -1,20 +1,22 @@
 from webbrowser import get
-from super_gradients.training import Trainer
+from super_gradients.training import Trainer, models
 from super_gradients.training.models import get as get_model
 from super_gradients.training.losses import PPYoloELoss
 from super_gradients.training.metrics import DetectionMetrics_050
-from super_gradients.training.models.detection_models.pp_yolo_e import (
-    PPYoloEPostPredictionCallback,
-)
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
 from super_gradients.training.dataloaders.dataloaders import (
     coco_detection_yolo_format_train,
     coco_detection_yolo_format_val,
 )
+from onemetric.cv.object_detection import ConfusionMatrix
+import supervision as sv
+from onemetric.cv.object_detection import MeanAveragePrecision
 from data.splitter.yolo import YOLO_Splitter
 import torch
 import yaml
 import json
 import os
+import numpy as np
 from ultralytics import NAS
 import re
 import pandas as pd
@@ -38,15 +40,16 @@ class Niche_YOLO_NAS:
         self.val_data = None
         self.test_data = None
 
-    def load(self, path_model=None):
+    def load(self,path_model=None):
         # self.model = YOLO(path_model)
         if path_model is None:
             self.model = NAS("yolo_nas_l")  # NAS(path_model)
         else:
-            self.model = get_model(
-                "yolo_nas_l", num_classes=80, checkpoint_path=path_model
+            self.model = models.get(
+                "yolo_nas_l", num_classes = 80, checkpoint_path=path_model
             )
         print("model %s loaded" % path_model)
+        return self.model
 
     def train(self, path_yaml, path_train_txt, path_val_txt, batch_size, num_epochs):
         with open(path_yaml, "r") as f:
@@ -74,21 +77,7 @@ class Niche_YOLO_NAS:
             },
             dataloader_params={"batch_size": batch_size, "num_workers": 2},
         )
-
-        # TODO: add test data
-        
-        self.test_data = coco_detection_yolo_format_val(
-            dataset_params={
-                "data_dir": os.path.dirname(path_val_txt),
-                "images_dir": "images",
-                "labels_dir": "labels",
-                "classes": list(range(num_classes)),
-            },
-            dataloader_params={"batch_size": batch_size, "num_workers": 2},
-        )
-
         ##############################
-        
         
         train_params = {
             "silent_mode": False,
@@ -104,7 +93,7 @@ class Niche_YOLO_NAS:
             "zero_weight_decay_on_bias_and_bn": True,
             "ema": True,
             "ema_params": {"decay": 0.9, "decay_type": "threshold"},
-            "max_epochs": 2,  ##
+            "max_epochs": num_epochs,  ##
             "mixed_precision": False,
             "loss": PPYoloELoss(
                 use_static_assigner=False, num_classes=num_classes, reg_max=16
@@ -133,35 +122,20 @@ class Niche_YOLO_NAS:
             valid_loader=self.val_data,
         )
 
-        best_model_path = os.path.join(self.dir_train, self.name_task, "ckpt_best.pth")
-        self.load(best_model_path)
-        ## TODO: use get function to load the best model 
+        ### best_model_path = os.path.join(self.dir_train, self.name_task, "ckpt_best.pth")
+        ### self.load(best_model_path)
+        
+        # TODO: use get function to load the best model 
         # reference: https://docs.deci.ai/super-gradients/documentation/source/QuickstartBasicToolkit.html
         #self.model = get_model(////
-        self.model = get_model("yolo_nas_l", num_classes=num_classes, checkpoint_path=best_model_path)
+        
+        ### self.model = get_model("yolo_nas_l", num_classes=num_classes, checkpoint_path=best_model_path)
         
         #########################################
 
-    def get_dataloader(self, path_yaml, data_path_txt, batch_size=16):
-        with open(path_yaml, "r") as f:
-            yaml_content = yaml.safe_load(f)
-        num_classes = yaml_content["nc"]
-        # print('num_classes', num_classes)
-        data = coco_detection_yolo_format_train(
-            dataset_params={
-                "data_dir": os.path.dirname(data_path_txt),
-                # os.path.join(os.path.split(path_train_txt)[0],'images'),
-                "images_dir": "images",
-                # os.path.join(os.path.split(path_train_txt)[0],'labels'),
-                "labels_dir": "labels",
-                #'classes': num_classes
-                "classes": list(range(num_classes)),
-            },
-            dataloader_params={"batch_size": batch_size, "num_workers": 2},
-        )
-        return data
+
     
-    def evaluate_trained_model(self, best_model, dataset_params):
+    def evaluate_trained_model(self, best_model, data_yaml_path, data_type = 'test'):
         """
         Evaluates a trained model on test data.
 
@@ -172,11 +146,54 @@ class Niche_YOLO_NAS:
         Returns:
         - The results of the test evaluation.
         """
+        with open(data_yaml_path, "r") as f:
+            yaml_content = yaml.safe_load(f)
+        num_classes = yaml_content["nc"]
+        # print('num_classes', num_classes)
+        if data_type == 'test' :
+            data_path_txt = self.dir_test
+            data = coco_detection_yolo_format_val(
+                dataset_params={
+                    "data_dir": os.path.dirname(data_path_txt),
+                    "images_dir": "images",
+                    "labels_dir": "labels",
+                    "classes": list(range(num_classes)),
+                },
+                dataloader_params={"batch_size": 16, "num_workers": 2},
+            )
+        elif data_type == 'train':
+            data_path_txt = self.dir_train
+            data = coco_detection_yolo_format_train(
+                dataset_params={
+                    "data_dir": os.path.dirname(data_path_txt),
+                    # os.path.join(os.path.split(path_train_txt)[0],'images'),
+                    "images_dir": "images",
+                    # os.path.join(os.path.split(path_train_txt)[0],'labels'),
+                    "labels_dir": "labels",
+                    #'classes': num_classes
+                    "classes": list(range(num_classes)),
+                },
+                dataloader_params={"batch_size": 16, "num_workers": 2},
+            )
+        elif data_type == 'val':
+            data_path_txt = self.dir_val
+            data = coco_detection_yolo_format_val(
+                dataset_params={
+                    "data_dir": os.path.dirname(data_path_txt),
+                    "images_dir": "images",
+                    "labels_dir": "labels",
+                    "classes": list(range(num_classes)),
+                },
+                dataloader_params={"batch_size": 16, "num_workers": 2},
+            )
+        else:
+            print('data_type is not valid')
+        
 
         test_metrics_list = DetectionMetrics_050(
             score_thres=0.1,
             top_k_predictions=300,
-            num_cls=len(dataset_params['classes']),
+            num_cls=len(list(range(num_classes))),
             normalize_targets=True,
             post_prediction_callback=PPYoloEPostPredictionCallback(
                 score_threshold=0.01,
@@ -185,14 +202,106 @@ class Niche_YOLO_NAS:
                 nms_threshold=0.7
             )
         )
+        
 
         return self.trainer.test(
             model=best_model,
             # Assuming you want to use the test_data from the class instance
-            test_loader=self.test_data,
-            test_metrics_list=test_metrics_list
+            test_loader = data,
+            test_metrics_list = test_metrics_list
         )
+        
+    def get_evaluation_matrix(self, best_model, data_yaml_path, data_type='test', conf=0.5, plot=True):
+        
+        
+        if data_type == 'test' :
+            data_dir = self.dir_test
+        elif data_type == 'train':
+            data_dir = self.dir_train
+        elif data_type == 'val':
+            data_dir = self.dir_val
+        ds = sv.DetectionDataset.from_yolo(images_directory_path=data_dir+"/images",annotations_directory_path=data_dir+"/labels",
+                                           data_yaml_path=data_yaml_path,force_masks=False)
+        
+        predictions = {}
+        for image_name, image in ds.images.items():
+            result = list(best_model.predict(image, conf=conf))[0]
+            detections = sv.Detections(
+                xyxy=result.prediction.bboxes_xyxy,
+                confidence=result.prediction.confidence,
+                class_id=result.prediction.labels.astype(int))
+            predictions[image_name] = detections
+            
+        keys = list(ds.images.keys())
+        annotation_batches, prediction_batches = [], []
 
+        for key in keys:
+            annotation = ds.annotations[key]
+            annotation_batch = np.column_stack((
+            annotation.xyxy,
+            annotation.class_id
+            ))
+            annotation_batches.append(annotation_batch)
+
+            prediction = predictions[key]
+            prediction_batch = np.column_stack((
+                prediction.xyxy,
+                prediction.class_id,
+                prediction.confidence
+                ))
+            prediction_batches.append(prediction_batch)
+           
+        confusion_matrix = ConfusionMatrix.from_detections(
+            true_batches=annotation_batches,
+            detection_batches=prediction_batches,
+            num_classes=len(ds.classes),
+            conf_threshold=conf
+            )
+        print("Confusion Matrix:", confusion_matrix.matrix)
+        
+        ### computing mean average precision
+        mean_average_precision = MeanAveragePrecision.from_detections(true_batches=annotation_batches,
+                                                                      detection_batches=prediction_batches,
+                                                                      num_classes=len(ds.classes),)
+        print("Mean Average Precision:", mean_average_precision.value)
+        ### computing other metrices
+        cm = confusion_matrix.matrix
+
+        precisions = []
+        recalls = []
+
+        num_classes = len(ds.classes)
+        for i in range(num_classes):
+            TP = cm[i, i]
+            FP = sum(cm[:, i]) - TP
+            FN = sum(cm[i, :]) - TP
+            TN = np.sum(cm) - (FP + FN + TP)
+
+            precision = TP / (TP + FP) if TP + FP != 0 else 0
+            recall = TP / (TP + FN) if TP + FN != 0 else 0
+
+            precisions.append(precision)
+            recalls.append(recall)
+
+        # If you want average precision and recall
+        avg_precision = sum(precisions) / num_classes
+        avg_recall = sum(recalls) / num_classes
+
+        print("Precision per class:", precisions[0])
+        print("Recall per class:", recalls[0])
+        print("Average Precision:", avg_precision)
+        print("Average Recall:", avg_recall)
+
+        if plot:
+            confusion_matrix.plot(os.path.join(data_dir, "confusion_matrix.png"), class_names=ds.classes)
+        else:
+            pass
+        
+        return confusion_matrix, mean_average_precision.value
+    
+   
+
+'''
 
 # Usage:
 # results = evaluate_trained_model(trainer_instance, best_model, test_data, dataset_params)
@@ -291,4 +400,4 @@ def extract_metrics(log_line):
             "F1@0.50": F1,
         }
     return None
-
+'''
